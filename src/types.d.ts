3883fa0bd24490d23c5df164b60f6e5ce1609b08
ulyana <novikova.ula.007@gmail.com>
2026-01-07 20:22:11 +0300

    added interfaces

---
+++ src/types.d.ts
@@ -0,0 +1,15 @@
+export interface IPost {
+    id: string,
+    title: string,
+    date: string,
+    text: string,
+}
+
+export interface IPostForm {
+    title: string,
+    text: string,
+}
+
+export interface IPostAPI {
+    [key: string]: IPostForm,
+}
